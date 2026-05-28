dd179df52788eae696688984367df2af7eda4ec8
jim2091 <jim2091@naver.com>
2026-05-28 11:38:55 +0900

    최초 생성

---
+++ semiprj/src/main/java/com/kh/semiprj/SemiprjApplication.java
@@ -0,0 +1,13 @@
+package com.kh.semiprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiprjApplication.class, args);
+	}
+
+}
