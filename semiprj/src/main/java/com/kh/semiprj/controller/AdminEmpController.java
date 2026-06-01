b8671d528d48c769d3badfb1ff1a33a9267ccb6f
myoung900516-diamond <sophie0516@gmail.com>
2026-06-01 17:36:03 +0900

    최초 사원 등록페이지구현

--- semiprj/src/main/java/com/kh/semiprj/controller/AdminEmpController.java
+++ semiprj/src/main/java/com/kh/semiprj/controller/AdminEmpController.java
@@ -24,9 +24,10 @@ public class AdminEmpController {
 	
 	@PostMapping("/register")
 	public String register(@ModelAttribute EmpDto empDto) {
+		System.out.println(empDto);
 		empDao.insertFromAdmin(empDto);
 		
-		return "redirect:./home";
+		return "redirect:/";
 		//홈으로 리다이렉트해놓았는데, 사원목록구현후 사원목록페이지로 리다이렉트할 예정입니다
 	}
 	
