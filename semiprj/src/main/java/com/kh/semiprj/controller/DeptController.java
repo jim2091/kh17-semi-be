52c15cb3f1fe8cc9fec4e0eeb1ab62cabfdc91f8
flysparrow76 <tnqkr071376@gmail.com>
2026-06-01 22:00:49 +0900

    부서 목록/컨트롤러

--- semiprj/src/main/java/com/kh/semiprj/controller/DeptController.java
+++ semiprj/src/main/java/com/kh/semiprj/controller/DeptController.java
@@ -1,9 +1,11 @@
 package com.kh.semiprj.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.semiprj.dao.DeptDao;
 import com.kh.semiprj.dto.DeptDto;
+import com.kh.semiprj.dto.EmpDto;
+import com.kh.spring09.exception.WhoAreYouException;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/dept")
@@ -20,13 +26,20 @@ public class DeptController {
 
 	//목록
 	@RequestMapping("/list")
-	public String list() {
+	public String list(Model model) {
+		List<DeptDto> list =deptDao.selectList();
+		model.addAttribute("list",list);
 		return "dept/list";
 	}
 	
 	//등록
 	@GetMapping("/insert")
-	public String insert() {
+	public String insert(HttpSession session) {
+		EmpDto loginUser = (EmpDto) session.getAttribute("loginUser");
+		if(loginUser == null || !loginUser.getEmpLevel().equals("관리자")){
+			throw new WhoAreYouException("관리자 권한이 필요한 기능입니다.");
+		}
+		
 		return "dept/insert";
 	}
 	@PostMapping("/insert")
