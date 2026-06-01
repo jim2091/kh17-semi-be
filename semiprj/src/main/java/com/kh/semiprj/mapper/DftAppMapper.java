367ac91924fa85c0cc71a286c240a71b7c3e0133
YSH0209 <yoonseunghoon0209@gmail.com>
2026-06-01 17:49:12 +0900

    mapper 작성중

--- semiprj/src/main/java/com/kh/semiprj/mapper/DftAppMapper.java
+++ semiprj/src/main/java/com/kh/semiprj/mapper/DftAppMapper.java
@@ -1,5 +1,19 @@
 package com.kh.semiprj.mapper;
 
-public class DftAppMapper {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import com.kh.semiprj.dto.DftAppDto;
+
+@Component
+public class DftAppMapper implements RowMapper<DftAppDto>{
+
+	@Override
+	public DftAppDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+		return null;
+	}
 
 }
--- semiprj/src/main/java/com/kh/semiprj/mapper/ExpAppMapper.java
+++ semiprj/src/main/java/com/kh/semiprj/mapper/ExpAppMapper.java
@@ -1,5 +1,19 @@
 package com.kh.semiprj.mapper;
 
-public class ExpAppMapper {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import com.kh.semiprj.dto.ExpAppDto;
+
+@Component
+public class ExpAppMapper implements RowMapper<ExpAppDto>{
+
+	@Override
+	public ExpAppDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+		return null;
+	}
 
 }
